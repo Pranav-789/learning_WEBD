6d668000da70c0a306436547ff450e297418fb29
Pranav-789 <pranavzambare2006@gmail.com>
2025-05-17 22:10:43 +0530

    Javascript Day1

---
+++ JAVASCRIPT/02_lect_1/02_data_type.js
@@ -0,0 +1,19 @@
+console.log("Chai");
+
+//
+
+/*
+
+*/
+
+// var score = 102;
+let score = 102; 
+let name = "Pranav";
+let isLoggedIn = false;
+
+//object
+let teaType = ["lemon tea", "orange tea", "oolong tea"];
+let user = {firstname: "Pranav", lastname: "Zambare"};
+
+let getScore = score;
+console.log(getScore);
---
+++ JAVASCRIPT/02_lect_1/03_changes.js
@@ -0,0 +1,7 @@
+let gameName  = "spider-man";
+
+gameName = "Bat-man";
+
+console.log(gameName);
+
+const userName = "pranavzambare";
